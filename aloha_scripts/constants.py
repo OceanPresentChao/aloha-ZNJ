49532a126e09c6d87967d8eda01ff2a58e86bbb5
OceanPresent <oceanpresent@163.com>
2024-12-03 19:08:18 +0800

    fix: align gripper joint

--- aloha_scripts/constants.py
+++ aloha_scripts/constants.py
@@ -1,110 +1,147 @@
 ### Task parameters
-#DATA_DIR = '<put your data dir here>'
+# DATA_DIR = '<put your data dir here>'
 
-DATA_DIR = '/root/interbotix_ws/src/aloha/aloha_scripts/data'
+DATA_DIR = "/root/interbotix_ws/src/aloha/aloha_scripts/data"
 TASK_CONFIGS = {
-    'aloha_wear_shoe':{
-        'dataset_dir': DATA_DIR + '/aloha_wear_shoe',
-        'num_episodes': 50,
-        'episode_len': 1000,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "aloha_wear_shoe": {
+        "dataset_dir": DATA_DIR + "/aloha_wear_shoe",
+        "num_episodes": 50,
+        "episode_len": 1000,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-    'operation_test':{
-        'dataset_dir': DATA_DIR + '/operation_test',
-        'num_episodes': 45,
-        'episode_len': 1400,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "operation_test": {
+        "dataset_dir": DATA_DIR + "/operation_test",
+        "num_episodes": 45,
+        "episode_len": 1400,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-    'exchange_cube':{
-        'dataset_dir': DATA_DIR + '/exchange_cube',
-        'num_episodes': 50,
-        'episode_len': 700,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "exchange_cube": {
+        "dataset_dir": DATA_DIR + "/exchange_cube",
+        "num_episodes": 50,
+        "episode_len": 700,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-        'put_cube':{
-        'dataset_dir': DATA_DIR + '/put_cube',
-        'num_episodes': 50,
-        'episode_len': 1100,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "put_cube": {
+        "dataset_dir": DATA_DIR + "/put_cube",
+        "num_episodes": 50,
+        "episode_len": 1100,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-        'red_cube':{
-        'dataset_dir': DATA_DIR + '/red_cube',
-        'num_episodes': 50,
-        'episode_len': 800,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "red_cube": {
+        "dataset_dir": DATA_DIR + "/red_cube",
+        "num_episodes": 50,
+        "episode_len": 800,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-        'blue_cube':{
-        'dataset_dir': DATA_DIR + '/blue_cube',
-        'num_episodes': 50,
-        'episode_len': 800,
-        'camera_names': ['cam_high', 'cam_low', 'cam_left_wrist', 'cam_right_wrist']
+    "blue_cube": {
+        "dataset_dir": DATA_DIR + "/blue_cube",
+        "num_episodes": 50,
+        "episode_len": 800,
+        "camera_names": ["cam_high", "cam_low", "cam_left_wrist", "cam_right_wrist"],
     },
-        'sim_insertion_human': {
-        'dataset_dir': DATA_DIR + '/sim_insertion_human',
-        'num_episodes': 50,
-        'episode_len': 500,
-        'camera_names': ['top']
+    "sim_insertion_human": {
+        "dataset_dir": DATA_DIR + "/sim_insertion_human",
+        "num_episodes": 50,
+        "episode_len": 500,
+        "camera_names": ["top"],
     },
 }
 
 ### ALOHA fixed constants
 DT = 0.02
-JOINT_NAMES = ["waist", "shoulder", "elbow", "forearm_roll", "wrist_angle", "wrist_rotate"]
-START_ARM_POSE = [0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239, 
-0, -0.96, 1.16, 0, -0.3, 0, 0.02239, -0.02239]
-
-MY_MASTER_START_ARM_POS = [0, -0.96, 1.16, 0, -0.3, 0.0, 0.02239, 0.3]
-
-# note: absolute path
-# Left finger position limits (qpos[7]), right_finger = -1 * left_finger
-MASTER_GRIPPER_POSITION_OPEN = 0.7041
-MASTER_GRIPPER_POSITION_CLOSE = -0.0598
-PUPPET_GRIPPER_POSITION_OPEN = -1.4572
-PUPPET_GRIPPER_POSITION_CLOSE = -2.261 
+JOINT_NAMES = [
+    "waist",
+    "shoulder",
+    "elbow",
+    "forearm_roll",
+    "wrist_angle",
+    "wrist_rotate",
+]
 
 # Gripper joint limits (qpos[6])
-MASTER_GRIPPER_JOINT_OPEN = 0.72
-MASTER_GRIPPER_JOINT_CLOSE = 0.1000
-PUPPET_GRIPPER_JOINT_OPEN = -1.6380
-PUPPET_GRIPPER_JOINT_CLOSE = -2.400
+# They are different according to your real robot
+MASTER_GRIPPER_JOINT_OPEN = 0.85136
+MASTER_GRIPPER_JOINT_CLOSE = -0.056757
+PUPPET_GRIPPER_JOINT_OPEN = -1.477224
+PUPPET_GRIPPER_JOINT_CLOSE = -2.584758
+
+PUPPET_START_ARM_POSE = [
+    0,
+    -0.96,
+    1.16,
+    0,
+    -0.3,
+    0,
+    PUPPET_GRIPPER_JOINT_OPEN,
+    -PUPPET_GRIPPER_JOINT_OPEN,
+    0,
+    -0.96,
+    1.16,
+    0,
+    -0.3,
+    0,
+    PUPPET_GRIPPER_JOINT_OPEN,
+    -PUPPET_GRIPPER_JOINT_OPEN,
+]
+
+MASTER_START_ARM_POSE = [
+    0,
+    -0.96,
+    1.16,
+    0,
+    -0.3,
+    0.0,
+    MASTER_GRIPPER_JOINT_OPEN,
+    -MASTER_GRIPPER_JOINT_OPEN,
+    0,
+    -0.96,
+    1.16,
+    0,
+    -0.3,
+    0.0,
+    MASTER_GRIPPER_JOINT_OPEN,
+    -MASTER_GRIPPER_JOINT_OPEN,
+]
 
 
 ############################ Helper functions ############################
 
-#将夹持器的位置值归一化到一个标准化的范围内
-MASTER_GRIPPER_POSITION_NORMALIZE_FN = lambda x: (x - MASTER_GRIPPER_POSITION_CLOSE) / (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE)
-PUPPET_GRIPPER_POSITION_NORMALIZE_FN = lambda x: (x - PUPPET_GRIPPER_POSITION_CLOSE) / (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE)
-#将已经归一化的夹持器位置值反归一化到实际的位置范围内
-MASTER_GRIPPER_POSITION_UNNORMALIZE_FN = lambda x: x * (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE) + MASTER_GRIPPER_POSITION_CLOSE
-PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN = lambda x: x * (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE) + PUPPET_GRIPPER_POSITION_CLOSE
-#将主控制器（Master）的位置值映射到从机（Puppet）的位置值
-MASTER2PUPPET_POSITION_FN = lambda x: PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN(MASTER_GRIPPER_POSITION_NORMALIZE_FN(x))
 
-#将夹持器关节位置归一化到一个标准化的范围内
-MASTER_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - MASTER_GRIPPER_JOINT_CLOSE) / (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE)
-#将给定的夹持器关节位置值归一化到特定范围内
-PUPPET_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - PUPPET_GRIPPER_JOINT_CLOSE) / (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE)
-#将已经归一化的夹持器关节位置值反归一化到实际的位置范围内
-MASTER_GRIPPER_JOINT_UNNORMALIZE_FN = lambda x: x * (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE) + MASTER_GRIPPER_JOINT_CLOSE
-#将归一化的关节位置值反转回原始范围内的值
-PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN = lambda x: x * (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE) + PUPPET_GRIPPER_JOINT_CLOSE
-#将主控制器（Master）的夹持器关节位置映射到从机Puppet）的夹持器关节位置
-MASTER2PUPPET_JOINT_FN = lambda x: PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN(MASTER_GRIPPER_JOINT_NORMALIZE_FN(x))
+# 将夹持器关节位置归一化到一个标准化的范围内
+MASTER_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - MASTER_GRIPPER_JOINT_CLOSE) / (
+    MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE
+)
+PUPPET_GRIPPER_JOINT_NORMALIZE_FN = lambda x: (x - PUPPET_GRIPPER_JOINT_CLOSE) / (
+    PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE
+)
+# 将已经归一化的夹持器关节位置值反归一化到实际的位置范围内
+MASTER_GRIPPER_JOINT_UNNORMALIZE_FN = (
+    lambda x: x * (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE)
+    + MASTER_GRIPPER_JOINT_CLOSE
+)
+PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN = (
+    lambda x: x * (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE)
+    + PUPPET_GRIPPER_JOINT_CLOSE
+)
+# 将主控制器（Master）的夹持器关节位置映射到从机Puppet）的夹持器关节位置
+MASTER2PUPPET_JOINT_FN = lambda x: PUPPET_GRIPPER_JOINT_UNNORMALIZE_FN(
+    MASTER_GRIPPER_JOINT_NORMALIZE_FN(x)
+)
 
-#将夹持器的速度值归一化到一个标准化的范围内
-MASTER_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE)
-#将给定的夹持器速度值归一化到特定范围内
-PUPPET_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE)
+# 求解两个值的平均数  1/2 （一个夹持器的开启位置或角度 + 夹持器的关闭位置或角度）
+MASTER_GRIPPER_JOINT_MID = (MASTER_GRIPPER_JOINT_OPEN + MASTER_GRIPPER_JOINT_CLOSE) / 2
 
-#将主控制器（Master）的位置值转换为夹持器关节位置值
-MASTER_POS2JOINT = lambda x: MASTER_GRIPPER_POSITION_NORMALIZE_FN(x) * (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE) + MASTER_GRIPPER_JOINT_CLOSE
-#将夹持器的关节位置值转换为主控制器的位置值。
-MASTER_JOINT2POS = lambda x: MASTER_GRIPPER_POSITION_UNNORMALIZE_FN((x - MASTER_GRIPPER_JOINT_CLOSE) / (MASTER_GRIPPER_JOINT_OPEN - MASTER_GRIPPER_JOINT_CLOSE))
 
-#根据这个参数计算出一个对应的关节位置
-PUPPET_POS2JOINT = lambda x: PUPPET_GRIPPER_POSITION_NORMALIZE_FN(x) * (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE) + PUPPET_GRIPPER_JOINT_CLOSE
-#将给定的关节位置 x 转换为对应的夹持器位置
-PUPPET_JOINT2POS = lambda x: PUPPET_GRIPPER_POSITION_UNNORMALIZE_FN((x - PUPPET_GRIPPER_JOINT_CLOSE) / (PUPPET_GRIPPER_JOINT_OPEN - PUPPET_GRIPPER_JOINT_CLOSE))
+MASTER_GRIPPER_POSITION_OPEN = 0.7041
+MASTER_GRIPPER_POSITION_CLOSE = -0.0598
+PUPPET_GRIPPER_POSITION_OPEN = -1.4572
+PUPPET_GRIPPER_POSITION_CLOSE = -2.261
 
-#求解两个值的平均数  1/2 （一个夹持器的开启位置或角度 + 夹持器的关闭位置或角度）
-MASTER_GRIPPER_JOINT_MID = (MASTER_GRIPPER_JOINT_OPEN + MASTER_GRIPPER_JOINT_CLOSE)/2
+# 将夹持器的速度值归一化到一个标准化的范围内
+MASTER_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (
+    MASTER_GRIPPER_POSITION_OPEN - MASTER_GRIPPER_POSITION_CLOSE
+)
+# 将给定的夹持器速度值归一化到特定范围内
+PUPPET_GRIPPER_VELOCITY_NORMALIZE_FN = lambda x: x / (
+    PUPPET_GRIPPER_POSITION_OPEN - PUPPET_GRIPPER_POSITION_CLOSE
+)
--- aloha_scripts/one_side_teleop.py
+++ aloha_scripts/one_side_teleop.py
@@ -1,18 +1,36 @@
 import time
 import sys
 import IPython
-e = IPython.embed
 
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 from interbotix_xs_msgs.msg import JointSingleCommand
-from constants import MASTER2PUPPET_JOINT_FN, DT, MY_MASTER_START_ARM_POS, START_ARM_POSE, MASTER_GRIPPER_JOINT_MID, PUPPET_GRIPPER_JOINT_CLOSE
-from robot_utils import get_arm_joint_positions, torque_on, torque_off, move_arms, move_grippers, get_arm_gripper_positions
+from constants import (
+    MASTER2PUPPET_JOINT_FN,
+    DT,
+    PUPPET_GRIPPER_JOINT_OPEN,
+    PUPPET_START_ARM_POSE,
+    MASTER_START_ARM_POSE,
+    MASTER_GRIPPER_JOINT_OPEN,
+)
+from robot_utils import (
+    get_arm_joint_positions,
+    torque_on,
+    torque_off,
+    move_arms,
+    move_grippers,
+    get_arm_gripper_positions,
+)
+
+e = IPython.embed
+
 
 def prep_robots(master_bot, puppet_bot):
     # reboot gripper motors, and set operating modes for all motors
     puppet_bot.dxl.robot_reboot_motors("single", "gripper", True)
     puppet_bot.dxl.robot_set_operating_modes("group", "arm", "position")
-    puppet_bot.dxl.robot_set_operating_modes("single", "gripper", "current_based_position")
+    puppet_bot.dxl.robot_set_operating_modes(
+        "single", "gripper", "current_based_position"
+    )
     master_bot.dxl.robot_set_operating_modes("group", "arm", "position")
     master_bot.dxl.robot_set_operating_modes("single", "gripper", "position")
     # puppet_bot.dxl.robot_set_motor_registers("single", "gripper", 'current_limit', 1000) # TODO(tonyzhaozh) figure out how to set this limit
@@ -20,16 +38,24 @@ def prep_robots(master_bot, puppet_bot):
     torque_on(master_bot)
 
     # move arms to starting position
-    start_arm_qpos = START_ARM_POSE[:6]
-    start_arm_qpos_master = MY_MASTER_START_ARM_POS[:6]
+    start_arm_qpos_puppet = PUPPET_START_ARM_POSE[:6]
+    start_arm_qpos_master = MASTER_START_ARM_POSE[:6]
 
-    #move_arms([master_bot, puppet_bot], [start_arm_qpos] * 2, move_time=1)
-    move_arms([master_bot, puppet_bot], [start_arm_qpos_master, start_arm_qpos], move_time=1)
+    # move_arms([master_bot, puppet_bot], [start_arm_qpos] * 2, move_time=1)
+    move_arms(
+        [master_bot, puppet_bot],
+        [start_arm_qpos_master, start_arm_qpos_puppet],
+        move_time=1,
+    )
 
     # move grippers to starting position,初始化位置
 
     # move_grippers([master_bot, puppet_bot], [MASTER_GRIPPER_JOINT_MID, PUPPET_GRIPPER_JOINT_CLOSE], move_time=0.5)
-    move_grippers([master_bot, puppet_bot], [MASTER_GRIPPER_JOINT_OPEN, PUPPET_GRIPPER_JOINT_OPEN], move_time=2)
+    move_grippers(
+        [master_bot, puppet_bot],
+        [MASTER_GRIPPER_JOINT_OPEN, PUPPET_GRIPPER_JOINT_OPEN],
+        move_time=2,
+    )
 
 
 def press_to_start(master_bot):
@@ -37,23 +63,35 @@ def press_to_start(master_bot):
     # disable torque for only gripper joint of master robot to allow user movement
     master_bot.dxl.robot_torque_enable("single", "gripper", False)
 
-    print(f'Close the gripper to start')
-    #close_thresh = -0.3
+    print(f"Close the gripper to start")
+    # close_thresh = -0.3
     close_thresh = 0.0
     pressed = False
     while not pressed:
         gripper_pos = get_arm_gripper_positions(master_bot)
         if abs(gripper_pos - close_thresh) < 0.03:
             pressed = True
-            kay = 1
-        time.sleep(DT/10)
+        time.sleep(DT / 10)
     torque_off(master_bot)
-    print(f'Started!')
+    print(f"Started!")
+
 
 def teleop(robot_side):
-    """ A standalone function for experimenting with teleoperation. No data recording. """
-    puppet_bot = InterbotixManipulatorXS(robot_model="vx300s", group_name="arm", gripper_name="gripper", robot_name=f'puppet_{robot_side}', init_node=True)
-    master_bot = InterbotixManipulatorXS(robot_model="wx250s", group_name="arm", gripper_name="gripper", robot_name=f'master_{robot_side}', init_node=False)
+    """A standalone function for experimenting with teleoperation. No data recording."""
+    puppet_bot = InterbotixManipulatorXS(
+        robot_model="vx300s",
+        group_name="arm",
+        gripper_name="gripper",
+        robot_name=f"puppet_{robot_side}",
+        init_node=True,
+    )
+    master_bot = InterbotixManipulatorXS(
+        robot_model="wx250s",
+        group_name="arm",
+        gripper_name="gripper",
+        robot_name=f"master_{robot_side}",
+        init_node=False,
+    )
 
     prep_robots(master_bot, puppet_bot)
     press_to_start(master_bot)
@@ -70,15 +108,15 @@ def teleop(robot_side):
         gripper_command.cmd = puppet_gripper_joint_target
         puppet_bot.gripper.core.pub_single.publish(gripper_command)
 
-        gripper_pos1 = get_arm_gripper_positions(puppet_bot)
-        gripper_pos2 = get_arm_gripper_positions(master_bot)
-        print("gripper_pos1", gripper_pos1)
-        print("gripper_pos2", gripper_pos2)
+        gripper_pos_puppet = get_arm_gripper_positions(puppet_bot)
+        gripper_pos_master = get_arm_gripper_positions(master_bot)
+        print("gripper_pos_puppet", gripper_pos_puppet)
+        print("gripper_pos_master", gripper_pos_master)
 
         # sleep DT
         time.sleep(DT)
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     side = sys.argv[1]
     teleop(side)
